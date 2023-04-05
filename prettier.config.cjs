be971ce21df596295be6232c1112551a34fff9fc
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-04-05 01:28:01 +0100

    feat: adds prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,31 @@
+/** @type {import('prettier').Options} */
+const config = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  bracketSameLine: true,
+  plugins: [
+    require.resolve('@ianvs/prettier-plugin-sort-imports'),
+    require.resolve('prettier-plugin-packagejson'),
+    require.resolve('prettier-plugin-jsdoc'),
+    require.resolve('prettier-plugin-tailwindcss'),
+  ],
+  importOrder: [
+    '^react',
+    '<TYPES>',
+    '<TYPES>^[./]',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@acme/(.*)$',
+    '',
+    '^@/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderMergeDuplicateImports: true,
+  pluginSearchDirs: false,
+};
+
+module.exports = config;
